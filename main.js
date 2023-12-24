58e58e62a8d3689a1309f4e1f0f5ce04b3318dff
VietHoang <hoanglv00256@gmail.com>
2023-12-24 18:17:12 +0700

    first commit

--- main.js
+++ main.js
@@ -17,7 +17,10 @@ let bt1 = (chuoi) => {
     }
     return result
 }
-console.log(bt1('play'));
+console.log(`bt1: ${bt1('play')}`);
+console.log(`bt1: ${bt1('playing')}`);
+console.log(`bt1: ${bt1('go')}`);
+
 
 //bt2
 function bt2(input) {
@@ -25,7 +28,7 @@ function bt2(input) {
       return character === character.toUpperCase() ? character.toLowerCase() : character.toUpperCase();
     });
   }
-console.log(bt2('TRung NghIa')); 
+console.log(`bt2: ${bt2('TRung NghIa')}`); 
 let bt2c2 = (chuoi) => {
     let mang = chuoi.split('')
     for(let index in mang){
@@ -38,7 +41,7 @@ let bt2c2 = (chuoi) => {
     }
     return mang.join('')
 }
-console.log(bt2c2('TRung NghIa'))
+console.log(`bt2c2: ${bt2c2('TRung NghIa')}`)
 //bt2c3
 let test = []
 let bt2c3 = (chuoi) => {
@@ -53,7 +56,7 @@ let bt2c3 = (chuoi) => {
         return accumulator
     },test).join('')
 }
-console.log(bt2c3('TRung NghIa'))
+console.log(`bt2c3: ${bt2c3('TRung NghIa')}`)
 // bt2c3
 let bt2c4 = (chuoi) => {
     let mang = chuoi.split('')
@@ -67,7 +70,7 @@ let bt2c4 = (chuoi) => {
         return item
     }).join('')
 }
-console.log(bt2c4('TRung NghIa'))
+console.log(`bt2c4 ${bt2c4('TRung NghIa')}`)
 //bt3
 let bt3 = (mang) => {
     return mang.filter((item) => {
@@ -75,7 +78,7 @@ let bt3 = (mang) => {
     })
 
 }
-console.log(bt3([1, "a", 2, "b", 3]))
+console.log(`bt3: ${bt3([1, "a", 2, "b", 3])}`)
 // bt4
 let bt4 = (mang) => {
     let length = mang.length;
@@ -91,25 +94,25 @@ let bt4 = (mang) => {
       }
     return mang;
 }
-console.log(bt4([1, 5, 3, 2]))
+console.log(`bt4: ${bt4([1, 5, 3, 2])}`)
 let bt4c2 = (mang) => {
     return mang.sort()
 }
-console.log(bt4c2([1, 5, 3, 2]))
+console.log(`bt4c2: ${bt4c2([1, 5, 3, 2])}`)
 // bt5
 let bt5 = (mang) =>{
     return mang.map((item) => {
         return item * 2
     })
 }
-console.log(bt5([1, 2, 3]))
+console.log(`bt5: ${bt5([1, 2, 3])}`)
 // bt6
 let bt6 = (mang) => {
     return mang.filter((item) => {
         return item % 2 !== 0
     })
 }
-console.log(bt6([1, 2, 3, 4, 5, 6, 7]))
+console.log(`bt6 ${bt6([1, 2, 3, 4, 5, 6, 7])}`)
 // bt7
 let bt7 = (mang) => {
     let result = []
@@ -121,5 +124,5 @@ let bt7 = (mang) => {
       }
     return result
 }
-console.log(bt7([1, 2, 3, 1, 2, 3, 4, 5, 3, 1, 2])
+console.log(`bt7: ${bt7([1, 2, 3, 1, 2, 3, 4, 5, 3, 1, 2])}`
 )
